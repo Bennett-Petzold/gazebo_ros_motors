0ea80f586c5f569df4b466c720dd36bd654cb20e
Bennett Petzold <bennettpetzold01@gmail.com>
2024-03-28 21:52:05 -0400

    Add partial ROS2 port

--- include/gazebo_ros_joint_motor/gazebo_ros_joint_motor.h
+++ include/gazebo_ros_joint_motor/gazebo_ros_joint_motor.h
@@ -6,91 +6,89 @@
 // Gazebo
 #include <gazebo/common/common.hh>
 #include <gazebo/physics/physics.hh>
-#include <gazebo_plugins/gazebo_ros_utils.h>
+// #include <gazebo_plugins/gazebo_ros_utils.h>
 
 // ROS
-#include <ros/ros.h>
-#include <std_msgs/Float32.h>
-#include <std_msgs/Int32.h>
-#include <sensor_msgs/JointState.h>
+// #include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <std_msgs/msg/int32.hpp>
 
 // Custom Callback Queue
-#include <ros/callback_queue.h>
-#include <ros/advertise_options.h>
+// #include <ros/advertise_options.h>
+// #include <ros/callback_queue.h>
 
 // Boost
-#include <boost/thread.hpp>
 #include <boost/bind.hpp>
+#include <boost/thread.hpp>
 
 namespace gazebo {
 
-  class Joint;
-  class Entity;
+class Joint;
+class Entity;
 
-  class GazeboRosMotor : public ModelPlugin {
+class GazeboRosMotor : public ModelPlugin {
 
-    public:
+public:
+  GazeboRosMotor();
+  ~GazeboRosMotor();
+  void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+  void Reset();
 
-      GazeboRosMotor();
-      ~GazeboRosMotor();
-      void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
-      void Reset();
+protected:
+  virtual void UpdateChild();
+  virtual void FiniChild();
 
-    protected:
+private:
+  GazeboRosPtr gazebo_ros_;
+  event::ConnectionPtr update_connection_;
+  physics::ModelPtr parent;
+  physics::JointPtr joint_;
+  physics::LinkPtr link_;
+  ros::Publisher encoder_publisher_;
+  ros::Publisher velocity_publisher_;
+  ros::Publisher joint_state_publisher_;
+  ros::Subscriber cmd_vel_subscriber_;
+  sensor_msgs::JointState joint_state_;
+  std::string wrench_frame_;
 
-      virtual void UpdateChild();
-      virtual void FiniChild();
+  // Topic params
+  std::string command_topic_;
+  std::string encoder_topic_;
+  std::string velocity_topic_;
+  bool publish_velocity_;
+  bool publish_encoder_;
+  bool publish_motor_joint_state_;
 
-    private:
+  // ODE
+  double input_;
+  double update_rate_;
+  double ode_joint_motor_fmax_;
+  double ode_joint_fudge_factor_;
 
-      GazeboRosPtr gazebo_ros_;
-      event::ConnectionPtr update_connection_;
-      physics::ModelPtr parent;
-      physics::JointPtr joint_;
-      physics::LinkPtr link_;
-      ros::Publisher encoder_publisher_;
-      ros::Publisher velocity_publisher_;
-      ros::Publisher joint_state_publisher_;
-      ros::Subscriber cmd_vel_subscriber_;
-      sensor_msgs::JointState joint_state_;
-      std::string wrench_frame_;
+  // Encoder model
+  int encoder_counter_;
+  int encoder_pulses_per_revolution_;
+  double encoder_to_shaft_ratio_;
 
-      // Topic params
-      std::string command_topic_;
-      std::string encoder_topic_;
-      std::string velocity_topic_;
-      bool publish_velocity_;
-      bool publish_encoder_;
-      bool publish_motor_joint_state_;
+  // Callback Queue
+  ros::CallbackQueue queue_;
+  std::thread callback_queue_thread_;
+  boost::mutex lock_;
+  void QueueThread();
 
-      // ODE
-      double input_;
-      double update_rate_;
-      double ode_joint_motor_fmax_;
-      double ode_joint_fudge_factor_;
+  // Helper variables
+  double update_period_;
+  common::Time last_update_time_;
 
-      // Encoder model
-      int encoder_counter_;
-      int encoder_pulses_per_revolution_;
-      double encoder_to_shaft_ratio_;
+  void publishRotorVelocity(double m_vel);
+  void publishEncoderCount(long ctr);
+  void cmdVelCallback(const std_msgs::Float32::ConstPtr &cmd_msg);
+  void publishWheelJointState(double m_vel, double m_wrench);
+  void publishEncoderCount(double m_vel, double dT);
+};
 
-      // Callback Queue
-      ros::CallbackQueue queue_;
-      std::thread callback_queue_thread_;
-      boost::mutex lock_;
-      void QueueThread();
-
-      // Helper variables
-      double update_period_;
-      common::Time last_update_time_;
-
-      void publishRotorVelocity(double m_vel);
-      void publishEncoderCount(long ctr);
-      void cmdVelCallback(const std_msgs::Float32::ConstPtr& cmd_msg);
-      void publishWheelJointState(double m_vel, double m_wrench);
-      void publishEncoderCount(double m_vel, double dT);
-  };
-
-}
+} // namespace gazebo
 
 #endif
